3f0fa2e7ba52f006168c843387f52b64b2a96525
Brennon Nuckols <47544816+brennuck@users.noreply.github.com>
2025-12-26 21:56:21 -0700

    .js

--- server/trpc/routers/elements.ts
+++ server/trpc/routers/elements.ts
@@ -1,5 +1,5 @@
 import { z } from "zod";
-import { router, protectedProcedure, publicProcedure } from "../trpc";
+import { router, protectedProcedure, publicProcedure } from "../trpc.js";
 import { TRPCError } from "@trpc/server";
 
 const elementTypeMap = {
